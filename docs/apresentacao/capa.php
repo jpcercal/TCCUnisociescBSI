e7786834b1e898ac5f3a71ff959f9f29721ed40f
João Paulo Cercal <sistemas@cekurte.com>
2014-06-17 12:40:42 -0300

    Capa da apresentação.

---
+++ docs/apresentacao/capa.php
@@ -0,0 +1,26 @@
+<?php
+
+$aluno          = "João Paulo Cercal";
+
+$orientadora    = "Prof. Msc. Leila Regina Techio";
+
+$apresentacao   = "Desenvolvimento de um Sistema Web de Simulado para a ZCPE";
+
+
+// ...
+
+
+foreach ($slides as $slide) {
+
+
+
+
+
+
+}
+
+
+// ...
+
+
+echo "Obrigado!";
