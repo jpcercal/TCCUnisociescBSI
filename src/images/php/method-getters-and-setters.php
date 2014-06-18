95714e32e6bce07ada1a9a03b42a5bec5c3ded3d
João Paulo Cercal <sistemas@cekurte.com>
2014-06-18 17:50:00 -0300

    Encapsulamento.

---
+++ src/images/php/method-getters-and-setters.php
@@ -0,0 +1,16 @@
+<?php
+
+class Carro
+{
+    private $cor;
+
+    public function getCor()
+    {
+        return $this->cor;
+    }
+
+    public function setCor($cor)
+    {
+        $this->cor = $cor;
+    }
+}
